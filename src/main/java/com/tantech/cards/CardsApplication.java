3159f892a8ea8b668e20e798c524d43d4b87debe
pfholden <pfholden@gmail.com>
2018-12-29 12:47:19 -0600

    Added search indexing function and basic card search functionality.

--- src/main/java/com/tantech/cards/CardsApplication.java
+++ src/main/java/com/tantech/cards/CardsApplication.java
@@ -1,12 +1,73 @@
 package com.tantech.cards;
 
+import com.tantech.cards.search.CardSearchService;
+import static java.lang.System.exit;
+import java.util.Scanner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CardsApplication {
+public class CardsApplication implements CommandLineRunner {
+    
+    @Autowired
+    private CardSearchService cardService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CardsApplication.class);
+//        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        
+        Scanner readInput = new Scanner(System.in);
+        
+        boolean done = false;
+        
+        while (!done){
+            System.out.println("Select: ");
+            System.out.println("  1. Search for cards");
+            System.out.println("  2. ReIndex data");
+            System.out.println("  3. Quit");
+            int n = readInput.nextInt();
+            switch(n){
+                case 1:
+                    // Search
+                    // Clear input line
+                    String name = readInput.nextLine();
+                    System.out.println("Enter search strings for the following prompts: ");
+                    System.out.print("Name: ");
+                    name = readInput.nextLine();
+                    System.out.print("Text: ");
+                    String text = readInput.nextLine();
+                    System.out.print("Type: ");
+                    String type = readInput.nextLine();
+                    System.out.print("Colors: ");
+                    String colors = readInput.nextLine();
+                    System.out.print("Set Name: ");
+                    String setName = readInput.nextLine();
+                    cardService.getluceneCards(name, text, type, colors, setName);
+                    break;
+                case 2:
+                    cardService.reloadIndex();
+                    break;
+                case 3:
+                    done = true;
+                default:
+            }
+        }
+        
+
+               
+// Below is to show how to use the Autowired service above.
+//        if (args.length > 0) {
+//            System.out.println(helloService.getMessage(args[0].toString()));
+//        } else {
+//            System.out.println(helloService.getMessage());
+//        }
+
+        exit(0);
+    }
 }
--- src/main/java/com/tantech/cards/db/CardRepository.java
+++ src/main/java/com/tantech/cards/db/CardRepository.java
@@ -14,8 +14,8 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Set;
-//import org.springframework.stereotype.Repository;
-//import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
@@ -25,18 +25,18 @@ public interface CardRepository extends CrudRepository<Card, Integer> {
     
     public Set<Card> findByName(String name);
     
-    public Card findByNameAndMtgSet(String name, MTGSet mtgSet);
-    
-    public List<Card> findByNameContaining(String name);
-    
-    public List<Card> findByNameContainingAndTextContaining(String name, String text);
-    
-    public List<Card> findByNameContainingAndTextContainingAndTypeContaining(String name, String text, String type);
-    
-    public List<Card> findByNameContainingAndTextContainingAndTypeContainingAndPowerBetweenAndToughnessBetweenAndCmcBetweenAndSetNameIn(
-            String name, String text, String type, Integer powerMin, Integer powerMax, Integer toughnessMin, 
-            Integer toughnessMax, Integer cmcMin, Integer cmcMax, Collection<String> setName);
-    
+//    public Card findByNameAndMtgSet(String name, MTGSet mtgSet);
+//    
+//    public List<Card> findByNameContaining(String name);
+//    
+//    public List<Card> findByNameContainingAndTextContaining(String name, String text);
+//    
+//    public List<Card> findByNameContainingAndTextContainingAndTypeContaining(String name, String text, String type);
+//    
+//    public List<Card> findByNameContainingAndTextContainingAndTypeContainingAndPowerBetweenAndToughnessBetweenAndCmcBetweenAndSetNameIn(
+//            String name, String text, String type, Integer powerMin, Integer powerMax, Integer toughnessMin, 
+//            Integer toughnessMax, Integer cmcMin, Integer cmcMax, Collection<String> setName);
+//    
     
     
 
